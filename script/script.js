ae0523144e76cbc0e9a6ce1522fa0e593f2df8a3
Gabriel Crispim <gabriel.crispim01@hotmail.com>
2023-04-02 18:12:12 -0300

    02/04/2023

--- script/script.js
+++ script/script.js
@@ -1,1 +1,31 @@
 AOS.init();
+
+const LanguageMap = {
+    'pt-BR': {
+        'quem-somos': 'Quem somos',
+        'equipe-tecnica': 'Equipe Técnica',
+        'servicos': 'Serviços',
+        'entre-em-contato':'Entre em contato'
+    },
+    'en': {
+        'quem-somos': 'Who we are',
+        'equipe-tecnica': 'Technical Team',
+        'servicos': 'Services',
+        'entre-em-contato':'Be in touch'
+    }
+};
+
+function updateLanguage(newLanguage) {
+    const elements = document.querySelectorAll('[data-i18n]');
+    elements.forEach(element => {
+        const key = element.getAttribute('data-i18n');
+        element.textContent = LanguageMap[newLanguage][key];
+    })
+}
+
+const LanguageButton = document.getElementById('button')
+
+LanguageButton.addEventListener('change', event => {
+    const newLanguage = event.target.value;
+    updateLanguage(newLanguage);
+})
