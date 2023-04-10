6d414c1202a6f6afec22d10f7a15cda2421b631a
Gabriel Crispim <gabriel.crispim01@hotmail.com>
2023-04-10 19:17:42 -0300

    10/04/2023

--- script/script.js
+++ script/script.js
@@ -5,13 +5,25 @@ const LanguageMap = {
         'quem-somos': 'Quem somos',
         'equipe-tecnica': 'Equipe Técnica',
         'servicos': 'Serviços',
-        'entre-em-contato':'Entre em contato'
+        'entre-em-contato':'Entre em contato',
+        'Sub-tittle1':'Inovações',
+        'Sub-tittle2':'e',
+        'Sub-tittle3':'Tecnologia',
+        'I1-H1':'Quem somos',
+        'I1-P1':'Somos uma STARTUP incubada no Polo Tecnológico e Criativo, conhecido também como Polo TeC, da Universidade Federal de Pernambuco (UFPE). A BIOTEC tem capacidade de desenvolver produto ou serviço inovador, voltado para as necessidades de cada cliente, como por exemplo: valorização de resíduos, biomassas, hidrogênio verde, biogás, biofertilizantes.',
+        'I1-P2':'Contamos com uma equipe experiente, qualificada, criativa, inovadora e disruptiva, buscando soluções eficientes e resultados consistentes para que nossos clientes possam enfrentar com melhores recursos a luta diárias da competitividade com sustentabilidade.',
     },
     'en': {
         'quem-somos': 'Who we are',
         'equipe-tecnica': 'Technical Team',
         'servicos': 'Services',
-        'entre-em-contato':'Be in touch'
+        'entre-em-contato':'Be in touch',
+        'Sub-tittle1':'Innovations',
+        'Sub-tittle2':'and',
+        'Sub-tittle3':'Technology',
+        'I1-H1':'Who we are',
+        'I1-P1':'We are a STARTUP incubated in the Technological and Creative Pole, also known as TeC Pole, of the Federal University of Pernambuco (UFPE). BIOTEC has the capacity to develop innovative product or service, focused on the needs of each customer, such as: waste valorization, biomass, green hydrogen, biogas, biofertilizers.',
+        'I1-P2':'We have an experienced, qualified, creative, innovative, and disruptive team, seeking efficient solutions and consistent results so that our clients can face the daily struggle of competitiveness with sustainability with better resources.',
     }
 };
 
