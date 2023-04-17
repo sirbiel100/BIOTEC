3710eedb555e3dd3a520ca9591f446987ae26e9b
Gabriel Crispim <gabriel.crispim01@hotmail.com>
2023-04-16 22:39:19 -0300

    16/04/2023

--- script/script.js
+++ script/script.js
@@ -11,6 +11,7 @@ const LanguageMap = {
         'Sub-tittle3':'Tecnologia',
         'I1-P1':'Somos uma STARTUP incubada no Polo Tecnológico e Criativo, conhecido também como Polo TeC, da Universidade Federal de Pernambuco (UFPE). A BIOTEC tem capacidade de desenvolver produto ou serviço inovador, voltado para as necessidades de cada cliente, como por exemplo: valorização de resíduos, biomassas, hidrogênio verde, biogás, biofertilizantes.',
         'I1-P2':'Contamos com uma equipe experiente, qualificada, criativa, inovadora e disruptiva, buscando soluções eficientes e resultados consistentes para que nossos clientes possam enfrentar com melhores recursos a luta diárias da competitividade com sustentabilidade.',
+        'I2-P1':'Animação',
         'I2-H3':'Competências',
         'I2-S-LI1':'Engenheira Química (UFPE)',
         'I2-S-LI2':'Doutora/Pesquisadora Industrial',
@@ -35,6 +36,7 @@ const LanguageMap = {
         'Sub-tittle3':'Technology',
         'I1-P1':'We are a STARTUP incubated in the Technological and Creative Pole, also known as TeC Pole, of the Federal University of Pernambuco (UFPE). BIOTEC has the capacity to develop innovative product or service, focused on the needs of each customer, such as: waste valorization, biomass, green hydrogen, biogas, biofertilizers.',
         'I1-P2':'We have an experienced, qualified, creative, innovative, and disruptive team, seeking efficient solutions and consistent results so that our clients can face the daily struggle of competitiveness with sustainability with better resources.',
+        'I2-P1':'Animation',
         'I2-H3':'Competences',
         'I2-S-LI1':'Chemical Engineer (UFPE)',
         'I2-S-LI2':'PhD/Industrial Researcher',
@@ -73,4 +75,17 @@ const navbar = document.querySelector('#nav')
 
 bttn.addEventListener('click', () => {
     navbar.classList.toggle('navbar-toggle')
+})
+
+// Animation Cards 
+
+const cardsButton = document.querySelector('#animation-button');
+const santulla = document.querySelector('#santulla')
+const adilson = document.querySelector('#adilson')
+const paulo = document.querySelector('#paulo')
+
+cardsButton.addEventListener('click', () =>  {
+    santulla.classList.toggle('santulla-off')
+    adilson.classList.toggle('adilson-off')
+    paulo.classList.toggle('paulo-off')
 })
